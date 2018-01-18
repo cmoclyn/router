d925ea8a0c0c0d7a996201929c9af2bfef83e550
Cédric MOCLYN <cmoclyn@dataone.fr>
2018-01-18 16:43:00 +0100

    Add a case to the RouteException

--- src/Exceptions/RouteException.php
+++ src/Exceptions/RouteException.php
@@ -5,8 +5,9 @@ namespace Router\Exceptions;
 use \Exceptions\Exception;
 
 class RouteException extends Exception{
-  const NO_ROUTES       = 0;
-  const NO_ROUTE_FOUND  = 1;
+  const NO_ROUTES             = 0;
+  const NO_ROUTE_FOUND        = 1;
+  const DIRECTORY_NOT_EXISTS  = 2;
 
   public function __construct($message, $value){
     switch($value){
@@ -16,6 +17,10 @@ class RouteException extends Exception{
         break;
       case self::NO_ROUTE_FOUND:
         $title  = "No route found";
+        $type   = "E";
+        break;
+      case self::DIRECTORY_NOT_EXISTS:
+        $title  = "Directory not exists";
         $type   = "E";
         break;
     }
